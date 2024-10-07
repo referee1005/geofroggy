5148c68059701f1859add7989353876135ec4e43
referee1005 <mixedfortune918@gmail.com>
2024-10-07 20:57:10 +0800

    change logic

--- pages/news/[id].js
+++ pages/news/[id].js
@@ -8,12 +8,17 @@ export default function Home() {
   const router = useRouter();
   const data = useSelector((state) => state.news.new);
   const dispatch = useDispatch();
-
-  const { id } = router.query;
+  // const [queryReady, setQueryReady] = useState(false);
+  // const { id } = router.query;
 
   useEffect(() => {
-    dispatch(fetchNewRequest(id));
-  }, [dispatch]);
+    if (router.isReady) {
+      const { id } = router.query;
+      if (id) {
+        dispatch(fetchNewRequest(id));
+      }
+    }
+  }, [router.isReady, router.query, dispatch]);
 
   return (
     <div className="mx-auto flex container-custom bg-custom-bg-primary">
